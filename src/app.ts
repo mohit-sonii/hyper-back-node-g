a526743265a4c7c031497891db1cf06ba2cd75a8
mohit-sonii <mohitsoni9731@gmail.com>
2025-05-29 11:25:58 +0530

    Added CORS

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.routes'
 import propertiRoutes from "./routes/property.routes"
 import favRoutes from './routes/favorite.routes'
 import recommendRoutes from './routes/recommend.route'
-
+import cors from 'cors'
 
 dotenv.config()
 const app = express();
@@ -14,26 +14,33 @@ app.use(express.json());
 app.use(cookieParser())
 
 const PORT = process.env.PORT || 5000;
+const localhost = process.env.LOCALHOST || '*'
+const deployedHost = process.env.DEPLOYEDHOST || '*'
 
+app.use(cors({
+    origin: [localhost, deployedHost],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}))
 
-app.use("/auth",authRoutes)
-app.use("/properties",propertiRoutes)
-app.use("/:user_id/fav",favRoutes)
-app.use("/recommend",recommendRoutes)
+app.use("/auth", authRoutes)
+app.use("/properties", propertiRoutes)
+app.use("/:user_id/fav", favRoutes)
+app.use("/recommend", recommendRoutes)
 
-const startServer = async()=>{
-    try{
+const startServer = async () => {
+    try {
         await connectToServer();
-        app.listen(PORT,()=>{
+        app.listen(PORT, () => {
             console.log(`App is listening to PORT : ${PORT}`)
         })
-    }catch(err){
+    } catch (err) {
         console.log(err);
         process.exit(1)
     }
 }
 
-const connectToServer = async()=>{
+const connectToServer = async () => {
     await mongoose.connect(`${process.env.DATABASE_URL}`)
     return;
 }
